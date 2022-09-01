95f5679ef79f2014602d13a3e3432833883645cd
rohittembhurne2194 <tembhurnerohit2194@gmail.com>
2022-09-01 15:14:56 +0530

    Comit By Rohit

---
+++ AppynittyWebApp/Models/News.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace AppynittyWebApp.Models
+{
+    public partial class News
+    {
+        public int Id { get; set; }
+        public DateTime NewsDate { get; set; }
+        public string NewsTitle { get; set; }
+        public string NewsEng { get; set; }
+        public string NewsMar { get; set; }
+        public bool? IsActive { get; set; }
+    }
+}
