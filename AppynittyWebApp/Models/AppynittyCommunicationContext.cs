99c7beda638c4a3726b65ff4f0f0e8e469342cf6
shubham1994s <shubhamkawle41@gmail.com>
2022-08-31 18:06:56 -0700

    comit by shubham

--- AppynittyWebApp/Models/AppynittyCommunicationContext.cs
+++ AppynittyWebApp/Models/AppynittyCommunicationContext.cs
@@ -23,6 +23,7 @@ namespace AppynittyWebApp.Models
         {
             if (!optionsBuilder.IsConfigured)
             {
+
 //#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
                 optionsBuilder.UseSqlServer("Server=202.65.157.254;Initial Catalog=AppynittyCommunication;Persist Security Info=False;User ID=appynitty;Password=BigV$Telecom;MultipleActiveResultSets=False;Connection Timeout=30;");
             }
