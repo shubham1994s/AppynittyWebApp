2fc444591b5c0674de22c4e263e4597b96023823
shubham1994s <shubhamkawle41@gmail.com>
2022-09-02 12:23:58 -0700

    comit by shubham

--- AppynittyWebApp/Models/AppynittyCommunicationContext.cs
+++ AppynittyWebApp/Models/AppynittyCommunicationContext.cs
@@ -33,7 +33,7 @@ namespace AppynittyWebApp.Models
         {
             if (!optionsBuilder.IsConfigured)
             {
-//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
                 optionsBuilder.UseSqlServer("Server=202.65.157.254;Initial Catalog=AppynittyCommunication;Persist Security Info=False;User ID=appynitty;Password=BigV$Telecom;MultipleActiveResultSets=False;Connection Timeout=30;");
             }
         }
@@ -191,10 +191,6 @@ namespace AppynittyWebApp.Models
 
             modelBuilder.Entity<News>(entity =>
             {
-                entity.HasNoKey();
-
-                //entity.Property(e => e.Id).ValueGeneratedOnAdd();
-
                 entity.Property(e => e.IsActive).HasColumnName("isActive");
 
                 entity.Property(e => e.NewsDate).HasColumnType("datetime");
--- AppynittyWebApp/Models/News.cs
+++ AppynittyWebApp/Models/News.cs
@@ -1,12 +1,10 @@
-﻿using Microsoft.EntityFrameworkCore;
-using System;
+﻿using System;
 using System.Collections.Generic;
 
 #nullable disable
 
 namespace AppynittyWebApp.Models
 {
-    [Keyless]
     public partial class News
     {
         public int Id { get; set; }
@@ -15,7 +13,5 @@ namespace AppynittyWebApp.Models
         public string NewsEng { get; set; }
         public string NewsMar { get; set; }
         public bool? IsActive { get; set; }
-
-
     }
 }
