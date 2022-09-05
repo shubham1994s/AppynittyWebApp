2b2adeb63da8fa00d814c54beb0f6c8264d5777a
rohittembhurne2194 <tembhurnerohit2194@gmail.com>
2022-09-05 14:43:59 +0530

    Comit By Rohit

--- AppynittyWebApp/Areas/Identity/Pages/Account/ResetPassword.cshtml.cs
+++ AppynittyWebApp/Areas/Identity/Pages/Account/ResetPassword.cshtml.cs
@@ -10,6 +10,10 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.WebUtilities;
+using Microsoft.IdentityModel.Protocols;
+using Microsoft.Data.SqlClient;
+using AppynittyWebApp.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace AppynittyWebApp.Areas.Identity.Pages.Account
 {
@@ -18,11 +22,17 @@ namespace AppynittyWebApp.Areas.Identity.Pages.Account
     {
         private readonly UserManager<AppynittyWebAppUser> _userManager;
 
+        private readonly AppynittyCommunicationContext _context;
+
         public ResetPasswordModel(UserManager<AppynittyWebAppUser> userManager)
         {
             _userManager = userManager;
         }
 
+        public ResetPasswordModel(AppynittyCommunicationContext context)
+        {
+            _context = context;
+        }
         [BindProperty]
         public InputModel Input { get; set; }
 
@@ -78,6 +88,29 @@ namespace AppynittyWebApp.Areas.Identity.Pages.Account
             var result = await _userManager.ResetPasswordAsync(user, Input.Code, Input.Password);
             if (result.Succeeded)
             {
+
+                // AspNetUser users = await _context.AspNetUsers.FindAsync(user.Id);
+
+                AspNetUser users = await _context.AspNetUsers.FirstOrDefaultAsync(m => m.Email == user.Email);
+
+                if (ModelState.IsValid)
+                {
+                    try
+                    {
+                        users.PasswordString = Input.Password;
+
+                        _context.Update(users);
+                        
+                      
+                        await _context.SaveChangesAsync();
+                    }
+                    catch (DbUpdateConcurrencyException)
+                    {
+                        throw;
+                    }
+                    return RedirectToAction(nameof(Index));
+                }
+
                 return RedirectToPage("./ResetPasswordConfirmation");
             }
 
