6a78f3b48e1f0b7aff99d2ed7d03700ce54d5023
umeshl@appynitty.com <umeshl@appynitty.com>
2022-09-07 13:33:16 +0530

    Chnages done From Side Its Millionaire persone and its me I will be Definetly Successfull in this year

---
+++ AppynittyWebApp/Controllers/ContactUs.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppynittyWebApp.Controllers
+{
+    public class ContactUs : Controller
+    {
+        public IActionResult Contact()
+        {
+            return View();
+        }
+        public IActionResult franchise()
+        {
+            return View();
+        }
+    }
+}
