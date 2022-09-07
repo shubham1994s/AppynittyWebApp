09dcf80582689dea06f906921f5797a077be8502
shubham1994s <shubhamkawle41@gmail.com>
2022-09-06 17:53:24 -0700

    comit by shubham

--- AppynittyWebApp/Models/NewsReplyDetails.cs
+++ AppynittyWebApp/Models/NewsReplyDetails.cs
@@ -1,18 +1,15 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace AppynittyWebApp.ViewModels
+namespace AppynittyWebApp.Models
 {
-    public class NewsRplyVM
+    [Keyless]
+    public class NewsReplyDetails
     {
-
-        public List<NewsRplyDetailsIteam> ListNewsRplyDetails { get; set; }
-    }
-
-    public class NewsRplyDetailsIteam
-    {
+      
         public int? News_Id { get; set; }
         public DateTime Date { get; set; }
         public string Name { get; set; }
