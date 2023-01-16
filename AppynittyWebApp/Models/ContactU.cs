23714d17552fd64e7f1070550206524a96f94ee6
rohittembhurne2194 <tembhurnerohit2194@gmail.com>
2023-01-16 18:33:11 +0530

    Comit By Rohit

--- AppynittyWebApp/Models/ContactU.cs
+++ AppynittyWebApp/Models/ContactU.cs
@@ -14,12 +14,18 @@ namespace AppynittyWebApp.Models
         public int Id { get; set; }
         [Column(TypeName = "datetime")]
         public DateTime Date { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string Email { get; set; }
         [Column("Mobile_No")]
+        [Required]
         public string MobileNo { get; set; }
+        [Required]
         public string Country { get; set; }
+        [Required]
         public string City { get; set; }
+        [Required]
         public string Business { get; set; }
     }
 }
